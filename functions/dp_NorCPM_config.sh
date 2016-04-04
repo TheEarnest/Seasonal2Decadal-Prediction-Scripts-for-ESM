1762c951440b2b510b77c58a9407662e7ae1f313
TheEarnest <earnestshen@gmail.com>
2016-04-04 11:56:34 +0200

    ud (@hex 2016/04/04@11:56)

--- functions/dp_NorCPM_config.sh
+++ functions/dp_NorCPM_config.sh
@@ -1,6 +1,6 @@
 # 2015/01/15 by Mao-Lin Shen
 set -e
-CASEDIR=NorCPM_ME
+CASEDIR='noresm1_ME_hist_p01'
 Ensembles='1 2 3 4 5'
 CaseConfig=ME
 #dailymicom=yes
@@ -28,8 +28,8 @@ VERSION=${CASEDIR}'_mem'
 #   an ensemble of run =same date multiple case name that finish by CASENAME_memXX
 ens_start=1 #1 means we start hybrid from an ensemble run
 ens_casename=noresm1_ME_hist_p01_mem
-ens_start_date=1995-05-01-00000
-branched_ens_date=1995-01-01-00000
+ens_start_date=2007-05-01-00000
+branched_ens_date=2007-01-01-00000
 ####Second Hybrid run possiblility :Historical start ####
 #   a historical run   =same case name multiple date (hist_start_date:hist_freq_date:NENS*hist_freq_date+hist_start_date)
 hist_start=0 #1 means we start hybrid from anstorical run
@@ -43,7 +43,7 @@ hist_freq_date=10
 SKIPASSIM=1
 SKIPPROP=1
 #start_date=1994-08-15-0000
-the_start_date=1995-05-01-00000
+the_start_date=2007-05-01-00000
 short_start_date=`echo $the_start_date | cut -c1-10`
 STARTMONTH=`echo $the_start_date | cut -c6-7`
 STARTYEAR=`echo $the_start_date | cut -c1-4` 
@@ -57,21 +57,24 @@ job_length=${forecast_length}
 
 if [ "${machine}" == "hexagon_intel" ]; then
   if [ "${CaseConfig}" == "ME" ] ; then
-    (( resubmit = forecast_length / 1 ))
-    if [ ${job_length} -gt 1 ]; then
-      job_length=1
-    fi
+#    (( resubmit = forecast_length / 1 ))
+#    if [ ${job_length} -gt 1 ]; then
+#      job_length=1
+#    fi
+    resubmit=0
+    (( jobmins = 40 * job_length ))
+    jobmins=${jobmins}
   elif [ "${CaseConfig}" == "F19tn21" ] ; then
     (( resubmit = forecast_length / 8 ))   
     if [ ${job_length} -gt 8 ]; then
       job_length=8
     fi
+    jobmins=59
   else
     echo "Don't know what you mean"
   #else
   #   echo "$CASEDIR not implemented in NorCPM, we quit"
   fi
-  jobmins=59
 else
   echo "I did not test it yet"
   if [ "${CaseConfig}" == "ME" ] ; then
